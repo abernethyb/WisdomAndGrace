dd4ffd5bd10943d5137464e1a4e609591b60196a
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-10-06 14:40:42 -0500

    created firebase project and linked it to app

--- WisdomAndGrace.sql
+++ WisdomAndGrace.sql
@@ -54,8 +54,8 @@ SET IDENTITY_INSERT [UserProfile] ON
 INSERT INTO [UserProfile]
   ([Id], [FirebaseUserId], [Name], [Email], [UserTypeId])
 VALUES
-  (1, 'MA9qsgE6vfbp2P1z0kv72bqRql43', 'Foo Barington', 'foo@bar.com', 1),
-  (2, 'vP3tkzRXWmRzwSLGwNTBS5fJs2N2', 'Bar Bazaar', 'bar@baz.com', 2);
+  (1, '0hNou2Wj1RbGemz3lr9olNjTVEH2', 'Bob Robertson', 'bob@bob.com', 1),
+  (2, 'aNTpVRkQPEWC2XD3kcEHBzHvvae2', 'Billy Williamson', 'billy@billy.com', 2);
 SET IDENTITY_INSERT [UserProfile] OFF
 
 
